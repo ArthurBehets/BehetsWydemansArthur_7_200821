54042e153ce0d55d0e186320a2e63be01aece504
Behets W. Arthur <arthur.behets24@gmail.com>
2021-08-21 16:07:46 +0200

    api begining

---
+++ backend/controllers/user.js
@@ -0,0 +1,14 @@
+
+
+
+exports.signup = (req, res, next) => {
+    //con.query("SELECT * FROM user", function (err, result, fields) {
+        //if (err) throw err;
+        //console.log(result);
+     // });
+     console.log("signup");
+}
+
+exports.login = (req, res, next) => {
+    
+}
