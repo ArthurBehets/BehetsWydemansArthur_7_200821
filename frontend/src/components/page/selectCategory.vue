49bcdd304bf56c8d04ab98b2a8cefdb298934dfc
Behets W. Arthur <arthur.behets24@gmail.com>
2021-09-17 13:36:52 +0200

    get all categories functions

--- frontend/src/components/page/selectCategory.vue
+++ frontend/src/components/page/selectCategory.vue
@@ -1,14 +1,22 @@
 <template>
    <div class="selectCategory">
         <ul id="categoryChoices">
+            <li><a>Tous</a></li>
+            <li v-for="category in  $store.state.categories " :key="category"><a v-on:click='getOneCategory(category)'> {{ category }} </a></li>
         </ul>
    </div>
 </template>
 
 <script>
+import { mapState } from 'vuex'
+
 export default ({
    name: "selectCategory",
+   computed: {
+        ...mapState(['count'])
+    },
    mounted: function(){
+       let categories =[];
        fetch('http://localhost:3000/api/article/getAllCategories')
        .then(function(res){
            if(res.ok){
@@ -17,14 +25,19 @@ export default ({
        })
        .then(function(values){
            let valuesBody = values.results;
+           console.log(valuesBody);
            for (let i in valuesBody){
-               document.getElementById("categoryChoices").innerHTML += "<li><a v-on:click='getOneCategory(" + valuesBody[i].categoryId + ")'>" + valuesBody[i].categoryName + "</a></li>"
-           }
+                categories.push(valuesBody[i].categoryName);
+            }
        })
+       .then(() =>{
+          this.$store.state.categories = categories; 
+       }
+       )
    },
    methods :{
-       getOneCategory : function(categoryId){
-           console.log("ok");
+       getOneCategory(categoryId){
+           console.log(categoryId);
            fetch("http://localhost:3000/api/article/getOneCategory/" + categoryId)
            .then(function(res){
                if(res.ok){
---
+++ frontend/src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+
+export default createStore({
+  state: {
+    categories : 'ok'
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
